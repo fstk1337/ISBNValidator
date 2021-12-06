02c25795f31acd946d936cb119aca1570d1e0a73
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2021-12-06 18:47:43 +0300

    Upload files

---
+++ src/com/fstk1337/isbntools/ValidateISBN.java
@@ -0,0 +1,11 @@
+package com.fstk1337.isbntools;
+
+public class ValidateISBN {
+    public boolean checkISBN(String isbn) {
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            total += isbn.charAt(i) * (10 - i);
+        }
+        return total % 11 == 0;
+    }
+}
---
+++ test/com/fstk1337/isbntools/ValidateISBNTest.java
@@ -0,0 +1,23 @@
+package com.fstk1337.isbntools;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidateISBNTest {
+
+    @Test
+    public void checkValidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue(result, "first value");
+        result = validator.checkISBN("0140177396");
+        assertTrue(result, "second value");
+    }
+
+    @Test
+    public void checkAnInvalidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+}
