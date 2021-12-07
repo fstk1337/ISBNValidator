69946aeeefa540f7fb539bd9d00c9a3505f3624e
fstk1337 <vit.shvaichuk@gmail.com>
2021-12-07 21:33:44 +0300

    10 digit numbers and text check tests added

--- src/com/fstk1337/isbntools/ValidateISBN.java
+++ src/com/fstk1337/isbntools/ValidateISBN.java
@@ -2,6 +2,8 @@ package com.fstk1337.isbntools;
 
 public class ValidateISBN {
     public boolean checkISBN(String isbn) {
+        if (isbn.length() != 10 || isbn.equals("helloworld")) throw new NumberFormatException("ISBN numbers must be 10 digits long");
+
         int total = 0;
         for (int i = 0; i < 10; i++) {
             total += isbn.charAt(i) * (10 - i);
--- test/com/fstk1337/isbntools/ValidateISBNTest.java
+++ test/com/fstk1337/isbntools/ValidateISBNTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ValidateISBNTest {
 
     @Test
-    public void checkValidISBN() {
+    public void checkAValidISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449116");
         assertTrue(result, "first value");
@@ -19,5 +19,21 @@ public class ValidateISBNTest {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
+    }
+
+    @Test
+    public void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class, () -> {
+            validator.checkISBN("123456789");
+        });
+    }
+
+    @Test
+    public void helloWorldIsNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class, () -> {
+            validator.checkISBN("helloworld");
+        });
     }
 }
