63fd9f866bc0e45c9ac4616f2407c73e56abc6c8
fstk1337 <vit.shvaichuk@gmail.com>
2021-12-11 17:12:49 +0300

    split ExternalService into two services

--- test/com/fstk1337/isbntools/StockManagementTests.java
+++ test/com/fstk1337/isbntools/StockManagementTests.java
@@ -7,15 +7,21 @@ public class StockManagementTests {
 
     @Test
     public void testCanGetACorrectLocatorCode() {
-        ExternalISBNDataService testService = new ExternalISBNDataService() {
-
+        ExternalISBNDataService testWebService = new ExternalISBNDataService() {
             @Override
             public Book lookup(String isbn) {
                 return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
             }
         };
+        ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return null;
+            }
+        };
         StockManager stockManager = new StockManager();
-        stockManager.setService(testService);
+        stockManager.setWebService(testWebService);
+        stockManager.setDatabaseService(testDatabaseService);
         String isbn = "0140177396";
         stockManager.getLocatorCode(isbn);
         String locatorCode = stockManager.getLocatorCode(isbn);
